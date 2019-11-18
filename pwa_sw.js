c5de5df3bec9ad3f94b1ce6cf4c639f43ce93578
hugodecasta <hugodecasta@orange.fr>
2019-11-18 14:55:45 +0100

    Init app files

---
+++ pwa_sw.js
@@ -0,0 +1,18 @@
+var CACHE_NAME = 'TeacherUtils-cache';
+var urlsToCache = [
+  '.'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+});
